f4f230585015527d6aa1d241ca9a037592d7a1f1
single-byte <70197335+single-byte@users.noreply.github.com>
2021-08-10 13:16:26 +0500

    index edited

--- js/app.js
+++ js/app.js
@@ -124,3 +124,21 @@ setInterval(() => {
         // auto_slides("popular-items");
     }, 500);
 }, 5000);
+
+// navbar sidebar
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+            $(".dropdown-btn i").css("transform", "rotate(0deg)")
+        } else {
+            dropdownContent.style.display = "block";
+            $(".dropdown-btn i").css({ "transform": "rotate(-180deg)", "padding-bottom": "5px" });
+        }
+    });
+}
